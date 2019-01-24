0d8a6ddd75d0f7f94bb9e943724a8a4a2e5cf132
Marcos Antonio Schratzenstaller <marksabbath@gmail.com>
2019-01-24 12:29:46 -0200

    Improves Author, Plugin URI.

--- plugin.php
+++ plugin.php
@@ -1,20 +1,17 @@
 <?php
 /*
+
 Plugin Name: SEO Data Transporter
-Plugin URI: https://github.com/copyblogger/seo-data-transporter
-
+Plugin URI: https://wordpress.org/plugins/seo-data-transporter/
 Description: SEO Data Transporter helps you transfer post/page specific SEO data, like custom doctitles, custom META descriptions and keywords, etc., from one platform (theme or plugin) to another.
-
-Author: StudioPress
-Author URI: http://www.studiopress.com/
-
 Version: 1.0.1
-
+Author: StudioPress
+Author URI: https://www.studiopress.com/
 Text Domain: seo-data-transporter
 Domain Path: /languages/
-
 License: GNU General Public License v2.0 (or later)
 License URI: http://www.opensource.org/licenses/gpl-license.php
+
 */
 
 require_once( plugin_dir_path( __FILE__ ) . 'seo-data-transporter.php' );
